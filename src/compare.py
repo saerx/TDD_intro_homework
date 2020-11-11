23d952cb7a86a2034fff70fa47623778214602a2
saerx <saerlaith.robyn@gmail.com>
2020-11-11 20:29:26 +0000

    refactored

--- src/compare.py
+++ src/compare.py
@@ -1,7 +1,7 @@
-def compare(num_1, num_2):
-    if num_1 < num_2:
-        return f"{num_1} is less than {num_2}"
-    elif num_1 > num_2:
-        return f"{num_1} is greater than {num_2}"
-    elif num_1 == num_2:
-        return f"{num_1} is equal to {num_2}"
+def compare(number_1, number_2):
+    if number_1 < number_2:
+        return f"{number_1} is less than {number_2}"
+    elif number_1 > number_2:
+        return f"{number_1} is greater than {number_2}"
+    else:
+        return f"{number_1} is equal to {number_2}"
--- tests/compare_test.py
+++ tests/compare_test.py
@@ -10,5 +10,6 @@ class TestCompare(unittest.TestCase):
     def test_compare_5_10_returns_5_is_less_than_10(self):
         self.assertEqual("5 is less than 10", compare(5,10))
     
+    # I picked these numbers for the test before looking at the solution weirdly
     def test_compare_4_4_returns_4_is_equal_to_4(self):
         self.assertEqual("4 is equal to 4", compare(4, 4))
